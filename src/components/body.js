7b0f6738d8637f35e5ee38f14dcde0a8cbb4b758
AnanyaMaruthi <ananyasheshu@gmail.com>
2021-06-02 19:22:03 +0530

    Minor changes

--- src/components/body.js
+++ src/components/body.js
@@ -8,9 +8,11 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Snackbar,
   Typography,
 } from "@material-ui/core";
 import placeholderImage from "../images/sample.jpg";
+import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
   flex: {
@@ -44,7 +46,89 @@ const useStyles = makeStyles((theme) => ({
 // 1. Add tool tip for disabled button
 // 2. Styling changes
 
-const objectLabels = ["Cat", "Dog", "Human", "Ball", "Sun"];
+const objectLabels = [
+  "BG",
+  "person",
+  "bicycle",
+  "car",
+  "motorcycle",
+  "airplane",
+  "bus",
+  "train",
+  "truck",
+  "boat",
+  "traffic light",
+  "fire hydrant",
+  "stop sign",
+  "parking meter",
+  "bench",
+  "bird",
+  "cat",
+  "dog",
+  "horse",
+  "sheep",
+  "cow",
+  "elephant",
+  "bear",
+  "zebra",
+  "giraffe",
+  "backpack",
+  "umbrella",
+  "handbag",
+  "tie",
+  "suitcase",
+  "frisbee",
+  "skis",
+  "snowboard",
+  "sports ball",
+  "kite",
+  "baseball bat",
+  "baseball glove",
+  "skateboard",
+  "surfboard",
+  "tennis racket",
+  "bottle",
+  "wine glass",
+  "cup",
+  "fork",
+  "knife",
+  "spoon",
+  "bowl",
+  "banana",
+  "apple",
+  "sandwich",
+  "orange",
+  "broccoli",
+  "carrot",
+  "hot dog",
+  "pizza",
+  "donut",
+  "cake",
+  "chair",
+  "couch",
+  "potted plant",
+  "bed",
+  "dining table",
+  "toilet",
+  "tv",
+  "laptop",
+  "mouse",
+  "remote",
+  "keyboard",
+  "cell phone",
+  "microwave",
+  "oven",
+  "toaster",
+  "sink",
+  "refrigerator",
+  "book",
+  "clock",
+  "vase",
+  "scissors",
+  "teddy bear",
+  "hair drier",
+  "toothbrush",
+];
 
 const Body = () => {
   const classes = useStyles();
@@ -53,8 +137,9 @@ const Body = () => {
   const [inputImage, setInputImage] = useState(placeholderImage);
   const [croppedImage, setCroppedImage] = useState(placeholderImage);
   const [outputImage, setOutputImage] = useState(placeholderImage);
-  const [objectLabel, setObjectLabel] = useState("Cat");
+  const [objectLabel, setObjectLabel] = useState("person");
   const [outputLoading, setOutputLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleFileUpload = (e) => {
     try {
@@ -89,6 +174,9 @@ const Body = () => {
       })
       .catch((error) => {
         console.error(error);
+        setOutputLoading(false);
+        setError(true);
+        setTimeout(() => setError(false), 3000);
       });
   };
 
@@ -177,6 +265,23 @@ const Body = () => {
           </Button>
         </div>
       </div>
+
+      <Snackbar
+        open={error}
+        autoHideDuration={6000}
+        onClose={() => {
+          setError(false);
+        }}
+      >
+        <Alert
+          onClose={() => {
+            setError(false);
+          }}
+          severity="error"
+        >
+          Oops! Something went wrong!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
